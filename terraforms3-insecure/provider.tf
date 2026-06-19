f8521933775ab0dd0f0b9ea5bf436b0bce165b1c
lankesh-koppisetti <78473387+lankesh-koppisetti@users.noreply.github.com>
2026-06-19 21:37:44 +0530

    Add AWS provider configuration for local testing

--- terraforms3-insecure/provider.tf
+++ terraforms3-insecure/provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
 }
